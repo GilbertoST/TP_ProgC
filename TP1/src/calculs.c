9dc0a380f92c240b512a8a64fb29e99dff84750e
Gilberto <129945131+GilbertoST@users.noreply.github.com>
2026-09-18 13:45:29 +0000

    Calculos

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+    int a, b;
+
+    // Équivalent de a = int(input())
+    scanf("%d", &a);
+
+    // Équivalent de b = int(input())
+    scanf("%d", &b);
+
+    // Équivalents de print()
+    printf("Somme : %d\n", a + b);
+    printf("Différence : %d\n", a - b);
+    printf("Division entière : %d\n", a / b);  // Attention, la division de 2 entiers en C est entière (ex: 5/2 = 2)
+    printf("Modulo : %d\n", a % b);
+
+    return 0;
+}
